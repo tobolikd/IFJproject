1c26cc612cad12163e74d34217bb90b6f34d27c5
David Tobolik <tobolikg@gmail.com>
2022-11-17 13:19:31 +0100

    feat: added malloc macro and moved some code

--- src/syn-analyzer.c
+++ src/syn-analyzer.c
@@ -2,14 +2,4 @@
 #include "syn-analyzer.h"
 #include "error-codes.h"
 
-bool isIntegrated(char *functionId)
-{
-    const char* integratedFunctions[] = { "reads", "readi", "readf", "write", "floatval", "intval", "strval", "strlen", "substring", "ord", "chr" };
 
-    for (long unsigned int i = 0; i < sizeof(integratedFunctions) / sizeof(char *); i++)
-    {
-        if (!strcmp(functionId, integratedFunctions[i]))
-            return true;
-    }
-    return false;
-}
