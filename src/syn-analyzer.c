91432dd8731173674f42e741129cea32b5dc098b
Mikynek <mikysek.jakub@gmail.com>
2022-11-18 23:30:18 +0100

    night update syn anal and grammar commit again

--- src/syn-analyzer.c
+++ src/syn-analyzer.c
@@ -1,5 +1,19 @@
 #include "lex-analyzer.h"
 #include "syn-analyzer.h"
+#include "symtable.h"
 #include "error-codes.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 
+enum ifjErrCode errorCode;
+
+void synAnalyser(TokenList *list)
+{
+    if (!list->TokenArray[0])
+    {
+        errorCode = SYNTAX_ERR;
+        return;
+    }
+    return;
+}
--- src/syn-analyzer.h
+++ src/syn-analyzer.h
@@ -1,6 +1,11 @@
 #ifndef IFJ_SYN_ANALYZER_H
 #define IFJ_SYN_ANALYZER_H
 
+#include "lex-analyzer.h"
+#include "syn-analyzer.h"
+#include "error-codes.h"
 #include "symtable.h"
+
+void synAnalyser(TokenList *list);
 
 #endif // IFJ_SYN_ANALYZER_H
