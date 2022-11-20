0597c60ab26ab0765e014712f65773789e6916e6
Fix1t <gbiel1117@gmail.com>
2022-11-20 15:32:33 +0100

    Added: Rules,  Structure for testing.

---
+++ src/preced-analyzer.c
@@ -0,0 +1,9 @@
+#include "preced-analyzer.h"
+#include "error-codes.h"
+#include "symtable.h"
+
+//for now returns boolean
+bool precedAnalyzer(TokenList *List, int index)
+{
+    return true;    
+}
