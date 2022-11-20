ab76a730e687a37c92d4401308c5212c80dd96c8
David Tobolik <tobolikg@gmail.com>
2022-11-20 18:52:15 +0100

    new: developer tools for easier debugging

---
+++ src/devel.h
@@ -0,0 +1,17 @@
+#ifndef IFJ_DEVEL_H
+#define IFJ_DEVEL_H
+
+#include <stdio.h>
+
+#include "error-codes.h"
+#include "ast.h"
+
+/* printAstItem
+ *
+ * prints AST item with type and data information to stderr
+ * active if DEBUG == 1
+ */
+void printAstItem(AST_item *item);
+
+
+#endif // IFJ_DEVEL_H
