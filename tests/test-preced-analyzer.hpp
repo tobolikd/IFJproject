56072d61306acd72d41058c7bf2c9041efdf7258
Fix1t <gbiel1117@gmail.com>
2022-11-20 15:32:33 +0100

    Added: Rules,  Structure for testing.

---
+++ tests/test-preced-analyzer.hpp
@@ -0,0 +1,58 @@
+#ifndef IFJ_TEST_PRECED_ANALYZER_H
+#define IFJ_TEST_PRECED_ANALYZER_H 1
+
+extern "C"
+{
+    #include "../src/lex-analyzer.h"
+    #include "../src/preced-analyzer.h"
+    #include "test-lex-analyzer.hpp" //prepTmpFile
+}
+
+#include <string>
+#include <iostream>
+#include <gtest/gtest.h>
+
+using std::tuple;
+using std::to_string;
+using std::get;
+using std::cerr;
+using std::vector;
+using std::pair;
+using std::endl;
+using std::string;
+using std::array;
+
+
+class testBase : public::testing::TestWithParam<tuple<bool,string>>
+{
+    protected:
+        FILE *tmpFile;
+        TokenList *testList;
+        bool expectedValue;
+        string dataIn;
+
+        void SetUp() override
+        {
+
+            expectedValue = get<0>(GetParam());
+            
+            dataIn = "<?php declare(strict_types=1);" + get<1>(GetParam());
+
+            tmpFile = prepTmpFile(dataIn.data());
+            ASSERT_FALSE(tmpFile == NULL) << "INTERNAL TEST ERROR - failed to allocate file." << endl;
+
+            testList = lexAnalyser(tmpFile);
+            ASSERT_FALSE(tmpFile == NULL) << "INTERNAL TEST ERROR - scanner failed to read input data." << endl;
+        }
+
+        void TearDown() override
+        {
+            if (tmpFile != NULL)
+                fclose(tmpFile);
+            listDtor(testList);
+        }
+};
+
+
+
+#endif // IFJ_TEST_PRECED_ANALYZER_H
