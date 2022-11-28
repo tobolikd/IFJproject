462b0a6932d61879d6244b9e42df5febdc4f8455
David Tobolik <tobolikg@gmail.com>
2022-11-28 13:14:12 +0100

    feat: #117, generating built ins, stack interface

--- src/code-gen-data.h
+++ src/code-gen-data.h
@@ -13,4 +13,20 @@ typedef struct {
     int labelNum;
 } code_block;
 
+/* genBuiltIns
+ *
+ * this function is called before code generation
+ * generate code for built in function
+ * label is the same as function name
+ *
+ * also generate functions for type conversion and type checks
+ */
+void genBuiltIns();
+
+/* genResolveCondition
+ *
+ * TODO comments
+ */
+void genResolveCondition();
+
 #endif // IFJ_CODE_GEN_DATA_H
